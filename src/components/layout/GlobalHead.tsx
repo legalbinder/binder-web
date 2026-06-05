dba61c5c77b7778a0b517196249acffd74be3694
Manuel <gonzales9u@gmail.com>
2026-06-04 22:51:42 -0500

    Update favicon assets

--- src/components/layout/GlobalHead.tsx
+++ src/components/layout/GlobalHead.tsx
@@ -5,7 +5,10 @@ export const GlobalHead = () => {
   return (
     <>
       <Helmet>
-        <link rel="icon" type="image/svg+xml" href="/lightmode_default_isotipo.svg" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512x512.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
       <SchemaMarkup type="organization" />
